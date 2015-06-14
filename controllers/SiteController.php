806147b8033a1078eda34420740ee716380ce918
Zsolti <lacza.zsolt@mostmarketing.hu>
2015-06-14 10:35:47 +0200

    test task fix

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -54,6 +54,10 @@ class SiteController extends Controller
 
     public function actionLogin()
     {
+		if (!Yii::$app->user->isGuest) {
+			return $this->redirect('jobadvertisement/index');
+		}
+
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
